df07e665c62d17365f00b6c3b9c2f2a5a4a63085
Stavinskaya_M <marina.st6277@gmail.com>
2023-12-14 23:01:18 +0600

    cypress2.3_API.2

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    projectId: "k7qm7z",
+    projectId: "34f629",
     baseUrl: "https://petstore.swagger.io/#/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
